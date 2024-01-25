e16be2211f7ba6f2d9bc85b9401b372f87eec68d
CaioMar7 <caiof.marques@yahoo.com.br>
2024-01-24 22:08:57 -0300

    Criada feature de unfavorite na lista de favoritos.

--- src/components/FavCard/index.tsx
+++ src/components/FavCard/index.tsx
@@ -1,21 +1,29 @@
+import { MouseEventHandler } from "react";
+
 import { UserProps } from "../../types/UserProps"
 
+import { MdFavorite } from "react-icons/md";
+
 type FavCardProps = {
-    data: UserProps
+    data: UserProps,
+    unFavUser: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const FavCard : React.FC<FavCardProps> = ({data} : FavCardProps) => {
+export const FavCard : React.FC<FavCardProps> = ({data, unFavUser} : FavCardProps) => {
 
     return (
         <>
-            <li key={data.id} className="w-full flex items-center gap-4 border-x-2 p-2 relative">
+            <li className="w-full flex items-center gap-4 border-x-2 p-2 relative">
                 <div className="w-20">
                     <img src={data.avatar_url} className="rounded-full" />
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col flex-1">
                     <h2 className="text-gray-100"> {data.name} </h2>
                     <a className="text-gray-400" href={`/${data.login}`}> Ver melhores repositórios </a>
                 </div>
+                <button className="justify-self-end" onClick={unFavUser}>
+                    <span className="text-3xl text-white"> <MdFavorite/> </span>
+                </button>
             </li>
         </>
     )
--- src/pages/FavoritesList/index.tsx
+++ src/pages/FavoritesList/index.tsx
@@ -13,66 +13,86 @@ import { UserProps } from "../../types/UserProps"
 
 export function FavoritesList() {
 
-    const navigate = useNavigate()
- 
-    const [isLoading, setIsLoading] = useState(false)
-    const [favoriteList, setFavoriteList] = useState<UserProps[] | null>(null)
-    const [userNotFound, setUserNotFound ] = useState<boolean | null>(null)
+  const navigate = useNavigate()
 
-    const handleBackPage = () => {
-        navigate(-1)
-    }
+  const [isLoading, setIsLoading] = useState(false)
+  const [favoriteList, setFavoriteList] = useState<UserProps[] | null>(null)
+  const [userNotFound, setUserNotFound] = useState<boolean | null>(null)
+
+  const handleBackPage = () => {
+    navigate(-1)
+  }
+
+  const handleUnFav = (login: string) : React.MouseEventHandler<HTMLButtonElement>  => {
+    return () => {
+      setFavoriteList((prevList) => {
+        if (prevList) {
+          const updatedList = prevList.filter((prev: UserProps) => login !== prev.login);
+          return updatedList;
+        }
+        return prevList;
+      });
+    };
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setUserNotFound(null);
+
+      try {
+        const storedFavoriteList = localStorage.getItem('@Gitfinder-favoriteList');
+        const parsedList = storedFavoriteList ? JSON.parse(storedFavoriteList) : [];
+
+        const promises = parsedList.map(async (favoritedUser: string) => {
+          try {
+            const response = await axios.get(`https://api.github.com/users/${favoritedUser}`);
+            const { id, name, login, avatar_url, followers, following } = response.data;
+
+            return { id, name, login, avatar_url, followers, following };
+          } catch (error) {
+            console.error(`Erro ao buscar dados para ${favoritedUser}:`, error);
+            return null;
+          }
+        });
+
+        const userList = await Promise.all(promises);
+        setFavoriteList(userList.filter((user) => user !== null) as UserProps[]);
+      } catch (error) {
+        setUserNotFound(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [])
 
     useEffect(() => {
-        const fetchData = async () => {
-          setIsLoading(true);
-          setUserNotFound(null);
-      
-          try {
-            const storedFavoriteList = localStorage.getItem('@Gitfinder-favoriteList');
-            const parsedList = storedFavoriteList ? JSON.parse(storedFavoriteList) : [];
-      
-            const promises = parsedList.map(async (favoritedUser: string) => {
-              try {
-                const response = await axios.get(`https://api.github.com/users/${favoritedUser}`);
-                const { id, name, login, avatar_url, followers, following } = response.data;
-                
-                return { id, name, login, avatar_url, followers, following };
-              } catch (error) {
-                console.error(`Erro ao buscar dados para ${favoritedUser}:`, error);
-                return null;
-              }
-            });
-      
-            const userList = await Promise.all(promises);
-            setFavoriteList(userList.filter((user) => user !== null) as UserProps[]);
-          } catch (error) {
-            setUserNotFound(true);
-          } finally {
-            setIsLoading(false);
-          }
-        };
-      
-        fetchData();
-      }, [])
+      let usersLogin : string[] = []
 
-    return (
-        <>
-            <div className="bg-gray-900">
-                <section className="md:w-4/12 my-0 mx-auto shadow-md bg-gray-800 max-h-screen min-h-screen overflow-y-auto">
-                    <div className="flex flex-col gap-6 p-4">
-                        <h1 className="font-bold text-3xl text-white flex items-center gap-4"> <button onClick={handleBackPage}> <MdOutlineArrowBackIos/> </button> Lista de usuários favoritos </h1>
-                        {isLoading ? <Loader/> : ""}
-                        {userNotFound && <h1> Usuário não encontrado </h1>}
+      favoriteList?.map((favUser) => usersLogin.push(favUser.login))
+      localStorage.setItem('@Gitfinder-favoriteList', JSON.stringify(usersLogin));
 
-                        <ul className="flex flex-col gap-6 p-4 overflow-y-auto">
-                            {favoriteList?.map((favUser) => 
-                              <FavCard data={favUser}/>
-                            )}
-                        </ul> 
-                    </div>
-                </section>
-            </div>
+    }, [favoriteList])
+
+  return (
+    <>
+      <div className="bg-gray-900">
+        <section className="md:w-4/12 my-0 mx-auto shadow-md bg-gray-800 max-h-screen min-h-screen overflow-y-auto">
+          <div className="flex flex-col gap-6 p-4">
+            <h1 className="font-bold text-3xl text-white flex items-center gap-4"> <button onClick={handleBackPage}> <MdOutlineArrowBackIos /> </button> Lista de usuários favoritos </h1>
+            {isLoading ? <Loader /> : ""}
+            {userNotFound && <h1> Usuário não encontrado </h1>}
+
+            <ul className="flex flex-col gap-6 p-4 overflow-y-auto">
+              {favoriteList?.map((favUser) =>
+                <FavCard key={favUser.id} data={favUser} unFavUser={handleUnFav(favUser.login)} />
+              )}
+            </ul>
+          </div>
+        </section>
+      </div>
     </>
-    )
+  )
 }
